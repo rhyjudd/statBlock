3e81300fbb6d929a68049265b2ced85f7b93a0f8
Rhyan <rhyjudd@gmail.com>
2021-01-17 12:00:38 -0600

    completed working stat block prototype

--- src/index.js
+++ src/index.js
@@ -38,13 +38,31 @@ class App extends React.Component{
   }
   rollStat(){
     let str = statRoller();
-    this.setState({strength: str})
+    let dex = statRoller();
+    let con = statRoller();
+    let int = statRoller();
+    let wis = statRoller();
+    let cha = statRoller();
+
+    this.setState({
+      strength: str,
+      dexterity: dex,
+      constitution: con,
+      intelligence: int,
+      wisdom: wis,
+      charisma: cha
+      })
   }
   render(){
     return  (    
       <div>
         <h1>DnD stat generator</h1>
-        <h3>Strength: {this.state.strength}</h3>
+        <h3>Strength:     {this.state.strength}</h3>
+        <h3>Dexterity:    {this.state.dexterity}</h3>
+        <h3>Constitution: {this.state.constitution}</h3>
+        <h3>Intelligence: {this.state.intelligence}</h3>
+        <h3>Wisdom:       {this.state.wisdom}</h3>
+        <h3>Charisma:     {this.state.charisma}</h3>
         <button onClick={this.rollStat}>Roll Stat</button>
       </div>
     );
